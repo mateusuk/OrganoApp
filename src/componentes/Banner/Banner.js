5dc29ec947421688ccb2e84038384f5a035918c0
Mateus de oliveira <mateus.oliveira.uk@gmail.com>
2024-04-24 16:33:47 +0100

    first commit

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,13 @@
+import "./Banner.css"
+
+function Banner(){
+    //JSX
+    return(
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="main banner"/>
+        </header>
+        
+    );
+}
+
+export default Banner
